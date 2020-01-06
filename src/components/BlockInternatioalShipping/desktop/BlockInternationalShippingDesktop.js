31e42f3ac2089756e808fb08d56b30631d3fc6f7
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2020-01-06 15:51:39 +0300

    add: internationalShipping

---
+++ src/components/BlockInternatioalShipping/desktop/BlockInternationalShippingDesktop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import css from 'components/BlockInternatioalShipping/desktop/BlockInternationalShippingDesktop.module.scss'
+import Layout from 'components/Layout/Layout'
+import logoRussia from 'static/russia-federal.svg'
+
+const BlockInternationalShippingDesktop = () => {
+  return (
+    <Layout className={css.container}>
+      <div className={css.img} style={{ backgroundImage: `url(${logoRussia})` }} />
+      <div className={css.block}>
+        <div className={css.title}>Перевозки за городом и по России</div>
+        <div className={css.text}>Междугородние перевозки, по России и республике Коми, а также в страны СНГ</div>
+      </div>
+    </Layout>
+  )
+}
+export default BlockInternationalShippingDesktop
--- src/containers/ContainerHome/desktop/ContainerHomeDesktop.js
+++ src/containers/ContainerHome/desktop/ContainerHomeDesktop.js
@@ -3,6 +3,8 @@ import Margin from 'components/Margin/Margin'
 import useHome from 'containers/ContainerHome/useHome'
 import BlockAdvantagesDesktop from 'components/BlockAdvantages/desktop/BlockAdvantagesDesktop'
 import BlockAchievementsDesktop from 'components/BlockAchievements/desktop/BlockAchievementsDesktop'
+import BlockInternationalShippingDesktop
+  from 'components/BlockInternatioalShipping/desktop/BlockInternationalShippingDesktop'
 
 const ContainerHomeDesktop = () => {
   const { advantagesList, achievementsList } = useHome()
@@ -12,6 +14,8 @@ const ContainerHomeDesktop = () => {
       <BlockAdvantagesDesktop list={advantagesList} />
       <Margin value={100} />
       <BlockAchievementsDesktop list={achievementsList} />
+      <Margin value={100} />
+      <BlockInternationalShippingDesktop />
     </>
   )
 }
